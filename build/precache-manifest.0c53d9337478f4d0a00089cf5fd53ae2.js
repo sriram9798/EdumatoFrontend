8c1a5da7b5e7e3056e2b3966beb4b877888b7581
Prasanna Venkatesan <prasanna1792@gmail.com>
2020-10-21 01:01:30 +0530

    Initial commit

---
+++ build/precache-manifest.0c53d9337478f4d0a00089cf5fd53ae2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c45f78114d5ff411f39068ce53dc52e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d8cfa1c961eb2c1949",
+    "url": "/static/css/2.47f38026.chunk.css"
+  },
+  {
+    "revision": "78d43802b2b5db4a6000",
+    "url": "/static/css/main.4bbb318e.chunk.css"
+  },
+  {
+    "revision": "15d8cfa1c961eb2c1949",
+    "url": "/static/js/2.f3b8d288.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f3b8d288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d43802b2b5db4a6000",
+    "url": "/static/js/main.d06fb65c.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  },
+  {
+    "revision": "f3c66c7f4d2e46b494ce07c0267c77be",
+    "url": "/static/media/1B167734-C2DA-4476-A73C-341BC905AC5F.f3c66c7f.png"
+  },
+  {
+    "revision": "3aed264f2ef785fe635cdc72d62706ad",
+    "url": "/static/media/C809C815-37EA-48FB-B898-E85D21D6BCEF.3aed264f.png"
+  },
+  {
+    "revision": "766ef1eb1ea7471018b7f4577a47b94a",
+    "url": "/static/media/breakfast.766ef1eb.jpg"
+  }
+]);
